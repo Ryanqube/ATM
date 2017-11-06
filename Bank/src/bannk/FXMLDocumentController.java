cc38865c1783c0aedef2993f21108aa219ba2d9d
Ryanqube <30398798+Ryanqube@users.noreply.github.com>
2017-11-06 10:37:46 +0700

    Add files via upload

---
+++ Bank/src/bannk/FXMLDocumentController.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bannk;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Nayang Albik B
+ */
+public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private JFXTextField pin;
+    @FXML
+    private JFXButton satu;
+    @FXML
+    private JFXButton dua;
+    @FXML
+    private JFXButton empat;
+    @FXML
+    private JFXButton lima;
+    @FXML
+    private JFXButton enam;
+    @FXML
+    private JFXButton tujuh;
+    @FXML
+    private JFXButton delapan;
+    @FXML
+    private JFXButton sembilan;
+    @FXML
+    private JFXButton hapus;
+    @FXML
+    private JFXButton sepuluh;
+    @FXML
+    private JFXButton ok;
+    @FXML
+    private JFXButton tiga;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void tekan1(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan2(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan4(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan5(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan6(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan7(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan8(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan9(ActionEvent event) {
+    }
+
+    @FXML
+    private void Hapus(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan0(ActionEvent event) {
+    }
+
+    @FXML
+    private void Hasil(ActionEvent event) {
+    }
+
+    @FXML
+    private void tekan3(ActionEvent event) {
+    }
+    
+}
